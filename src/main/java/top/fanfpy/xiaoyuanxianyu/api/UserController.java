871df4906e34e5e066be2353724b007b8241f86b
fanfpy <2324802641@qq.com>
2018-04-19 18:20:19 +0800

    test

--- src/main/java/top/fanfpy/xiaoyuanxianyu/api/UserController.java
+++ src/main/java/top/fanfpy/xiaoyuanxianyu/api/UserController.java
@@ -13,44 +13,30 @@ import java.util.Optional;
  * @date 2018/04/18
  * */
 @RestController
-@RequestMapping(value = "/api")
+@RequestMapping(value = "/api/user")
 public class UserController {
 
+    private final UserRepository userRepository;
+
     @Autowired
-    private UserRepository userRepository;
+    public UserController(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
 
     /**
      * 获取全部的user信息
      * */
-    @GetMapping(value = "/user")
+    @GetMapping(value = "/")
     public List<User> getUsers(){
         return userRepository.findAll();
     }
-    /**
-     * 通过id获取用户信息
-     * */
-    @GetMapping(value = "user/{id}")
-    public Optional<User> getOneUser(@PathVariable("id") Integer id){
-        return userRepository.findById(id);
-    }
 
-    @GetMapping(value = "user/name/{name}")
-    public List<User> getUserName(@PathVariable("name") String name){
-        return userRepository.findByUsername(name);
-    }
 
-   /**
-    * 通过id删除用户
-    * */
-    @DeleteMapping(value = "user/{id}")
-    public void delUser(@PathVariable("id") Integer id){
-        userRepository.deleteById(id);
-    }
 
     /**
-     * 添加用户
+     * 添加一个新用户
      * */
-    @PostMapping(value = "/user")
+    @PostMapping(value = "/")
     public User addUser(@RequestParam("phone") String phone , @RequestParam("username") String username,
                         @RequestParam("password") String password,@RequestParam("qq") String qq,
                         @RequestParam("createAt") String createAt,@RequestParam("goodNum") Integer goodNum,
@@ -68,6 +54,60 @@ public class UserController {
         user.setStatus(status);
         return  userRepository.save(user);
     }
+
+    /**
+     * 通过id获取用户信息
+     * */
+    @GetMapping(value = "/{id}")
+    public Optional<User> getOneUser(@PathVariable("id") Integer id){
+        return userRepository.findById(id);
+    }
+
+    /**
+     *通过id更新用户
+     * */
+    @PutMapping(value = "/{id}")
+    public User upateUser(@PathVariable("id") Integer id,@RequestParam("phone") String phone ,
+                          @RequestParam("username") String username, @RequestParam("password") String password,
+                          @RequestParam("qq") String qq, @RequestParam("createAt") String createAt,
+                          @RequestParam("goodNum") Integer goodNum, @RequestParam("power") Byte power,
+                          @RequestParam("lastLogin") String lastLogin, @RequestParam("status") Byte status){
+        User user = new User();
+        user.setId(id);
+        user.setPhone(phone);
+        user.setUsername(username);
+        user.setPassword(password);
+        user.setQq(qq);
+        user.setCreateAt(createAt);
+        user.setGoodsNum(goodNum);
+        user.setPower(power);
+        user.setLastLogin(lastLogin);
+        user.setStatus(status);
+        return userRepository.save(user);
+    }
+
+
+    /**
+     * 通过id删除用户
+     * */
+    @DeleteMapping(value = "/{id}")
+    public void delUser(@PathVariable("id") Integer id){
+        userRepository.deleteById(id);
+    }
+
+
+    /**
+     *  通过name获取用户信息
+    * */
+    @GetMapping(value = "/name/{name}")
+    public List<User> getUserName(@PathVariable("name") String name){
+        return userRepository.findByUsername(name);
+    }
+
+
+
+
+
 
 
 }
